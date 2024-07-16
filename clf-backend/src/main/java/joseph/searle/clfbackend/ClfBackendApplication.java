72d97873b190f6558b9705c4e1e76a4468d46b08
Joseph Searle <josephwilliamsearle@gmail.com>
2024-07-16 13:32:31 +0100

    Initialized Spring back-end and React front-end modules

---
+++ clf-backend/src/main/java/joseph/searle/clfbackend/ClfBackendApplication.java
@@ -0,0 +1,13 @@
+package joseph.searle.clfbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClfBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClfBackendApplication.class, args);
+    }
+
+}
---
+++ clf-backend/src/test/java/joseph/searle/clfbackend/ClfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package joseph.searle.clfbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClfBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
